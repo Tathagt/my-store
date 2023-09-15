2c936b51ae0fab8a5abaed848a7ebe2d11b6ffbe
Tathagt <112072412+Tathagt@users.noreply.github.com>
2023-09-16 02:06:50 +0530

    Done

--- components/Related.jsx
+++ components/Related.jsx
@@ -24,7 +24,10 @@ const Related = () => {
   return (
     <div className='mt-[50px] md:mt-[100px] mb-[100px] md:mb-0'>
         <div className='text-2xl font-bold mb-5'>You Might Also Like</div>
-        <Carousel responsive={responsive}>
+        <Carousel responsive={responsive}
+        containerClass='mx-[10px]'
+        itemClass='px-[10px]'
+        >
             <Product/>
             <Product/>
             <Product/>
